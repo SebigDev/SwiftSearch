7747ad1cb29619d2ad2236783108437652d4feb7
Chukwunasa Igwe <chooksigwe@outlook.com>
2018-06-17 23:39:59 +0100

    Add project files.

---
+++ SwiftSearch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SwiftSearch.Startup))]
+namespace SwiftSearch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
